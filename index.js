34d7be7811912eae15e6b4b1beb75ae506605fc1
Ramesh Banakar <ramesh.s.b@campusuvce.in>
2022-10-24 12:21:50 +0530

    rate limiter to api request

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const errorHandler = require("./middleWare/errorHandler");
 const bodyParser = require("body-parser");
+const rateLimit=require("express-rate-limit")
 const cors = require("cors");
 const mongoose = require("mongoose");
 const passportJWT = require("./middleWare/passportJWT")();
@@ -19,6 +20,17 @@ const followRoute = require("./Route/follow");
 app.use(bodyParser.json());
 app.use(express.json())
 app.use(cors());
+
+// const limiter = rateLimit({
+// 	windowMs: 15 * 1000, // 15 seconds
+// 	max: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+// 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+// 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+// })
+
+// Apply the rate limiting middleware to all requests
+//app.use(limiter)
+
 app.use("/api/post",passportJWT.authenticate(),postRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/follow",passportJWT.authenticate(),followRoute)
