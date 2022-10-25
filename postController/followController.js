93c3c7bc5fbb22a47f2b70cffb5d1cf801db6852
Ramesh Banakar <ramesh.s.b@campusuvce.in>
2022-10-25 08:53:10 +0530

    small change

--- postController/followController.js
+++ postController/followController.js
@@ -1,8 +1,10 @@
 const User = require("../models/user");
+//const redisClient=require("../config/redis").getClient();
 exports.follow = async (req, res, next) => {
   try {
     req.user.following.push(req.params.id);
     req.user.save();
+    //redisClient.hdel("user",req.user.id)
     res.send({ message: "success" });
   } catch (err) {
     next(err);
